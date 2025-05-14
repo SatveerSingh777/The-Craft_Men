dd8b449b628e0fcdde5e2608fca23c343a800398
SatveerSingh777 <satveersinghsingh777@gmail.com>
2025-05-14 14:01:48 +0530

    changes

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import './Contact.css'
 function Contact() {
+  const phoneNumber = "919877946435";
+  const message = "Hello! I would like to connect with you.";
   return (
     <>
       <section className="Contact-section">
         <div className="Con-container">
-            <h1>THANK YOU ..</h1>
-            <div className="contact">
-                <p>E-Mail: Thecraftmen01@gmail.com</p>
-                <p>Whatsapp-no: +91-98779-46435</p>
-            </div>
+          <h1>THANK YOU ..</h1>
+          <div className="contact">
+
+            <p>E-Mail: <a href="mailto:Thecraftmen01@gmail.com?subject=Hello&body=How are you?" target="_blank" rel="noopener noreferrer">Thecraftmen01@gmail.com</a></p>
+            <p><a
+              href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: 'none', color: 'green' }}
+            >
+              Click to Chat on WhatsApp
+            </a></p>
+
+          </div>
         </div>
       </section>
     </>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Navbar.css'
 
+
 function Navbar() {
+  // const contact = () => {
+  //   window.open("_blank");
+  // };
   return (
+
     <>
      <nav>
         <div className="logo">
             <h1>THE</h1>
             <p>Craft Men</p>
         </div>
-    
+          {/* <button onClick={contact}>Contact-Us</button> */}
         </nav> 
     </>
   )
